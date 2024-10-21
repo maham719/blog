63853c7611d5860b8ca974c6ff6a98d26c3066e7
Syeda Maham Amjad <163040767+maham719@users.noreply.github.com>
2024-10-22 02:40:44 +0500

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -14,6 +14,29 @@ import { useState } from 'react';
 import Link from 'next/link';
 
 export default function Header() {
+
+  const [isScrolled, setIsScrolled] = useState(false);
+   
+    useEffect(() => {
+        const handleScroll = () => {
+    const nav =document.querySelector(".header");
+
+          if (window.scrollY > 0) {
+            setIsScrolled(true);
+            nav?.classList.add("navbar");
+          } else {
+            setIsScrolled(false);
+            nav?.classList.remove("navbar");
+          }
+        };
+    
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+          window.removeEventListener("scroll", handleScroll);
+        };
+      }, []);
+
+  
     const [menuOpen, setMenuOpen] = useState(false);
 
     useEffect(() => {
